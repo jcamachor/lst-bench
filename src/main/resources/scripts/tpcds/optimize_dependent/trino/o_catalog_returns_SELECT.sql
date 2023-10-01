be91a22437f5b2d8ca2eb678c588aa674c922877
Jesus Camacho Rodriguez <jesusca@microsoft.com>
2023-10-01 12:26:48 -0700

    aliases

--- src/main/resources/scripts/tpcds/optimize_dependent/trino/o_catalog_returns_SELECT.sql
+++ src/main/resources/scripts/tpcds/optimize_dependent/trino/o_catalog_returns_SELECT.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT cr_returned_date_sk
+SELECT DISTINCT cr_returned_date_sk AS cr_returned_date_sk
 FROM ${catalog}.${database}.catalog_returns
 WHERE cr_returned_date_sk IS NOT NULL
 ORDER BY cr_returned_date_sk ASC;
